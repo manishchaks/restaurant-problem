07c57b677a2336166cde5900211a52e9266a31fe
Manish Chakravarty <manishchaks@gmail.com>
2016-12-24 17:03:53 +0530

    adding spec for invalid line item for Order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,6 +9,12 @@ describe Order do
         order.add_line_item(line_item_hash)
         expect(order.line_items.size).to eql(1)
       end
+      it "should raise an exception if line item does not specify valid food type" do
+        order = Order.new
+        line_item_hash = Hash.new
+        line_item_hash[:foo] = 10
+        expect {order.add_line_item(line_item_hash)}.to raise_error(RuntimeError,'Food type can only be one of [:regular, :fish_free, :gluten_free, :vegetarian]')
+      end
     end
   end
 end
